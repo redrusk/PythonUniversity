811cf409fade363555abda8a9df9d82bde51ebdf
Evgeniy Okhov <joeyjortravisbar@ya.ru>
2026-06-15 22:37:24 +0300

    add tasks for lesson 3

---
+++ lesson-3/task-1.py
@@ -0,0 +1,5 @@
+pet_type = input("Введите вид питомца: ")
+pet_age = input("Введите его возраст: ")
+pet_name = input("Введите его кличку: ")
+
+print(f"Это {pet_type} по кличке \"{pet_name}\". Возраст: {pet_age} года.")
---
+++ lesson-3/task-2.py
@@ -0,0 +1,15 @@
+print("Перечислите этапы развития человека")
+australopithecus = input("Введите первый этап: ")
+homo_habilis = input("Введите второй этап: ")
+homo_erectus = input("Введите третий этап: ")
+home_neanderthalensis = input("Введите четвертый этап: ")
+home_sapiens_sapiens = input("Введите пятый этап: ")
+
+print(
+    australopithecus,
+    homo_habilis,
+    homo_erectus,
+    home_neanderthalensis,
+    home_sapiens_sapiens,
+    sep="=>",
+)
